9749ae52bde3be82b8e525d13ccc6f139759bf54
mattkim17 <68535436+mattkim17@users.noreply.github.com>
2021-06-13 18:14:24 -0500

    Add files via upload

---
+++ st7789.c
@@ -0,0 +1,245 @@
+// functions to operate the ST7789 on the PIC32
+// adapted from https://github.com/sumotoy/TFT_ST7789
+// and https://github.com/adafruit/Adafruit-ST7789-Library
+
+// pin connections:
+// GND - GND
+// VCC - 3.3V
+// SCL - B14
+// SDA - B13
+// RES - B15
+// DC - B12
+// BLK - NC
+
+#include <xc.h>
+#include "ST7789.h"
+#include "spi.h"
+#include "font.h"
+#include <stdio.h>
+#include <string.h>
+
+// DEVCFG0
+#pragma config DEBUG = OFF // disable debugging
+#pragma config JTAGEN = OFF // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // disable flash write protect
+#pragma config BWP = OFF // disable boot write protect
+#pragma config CP = OFF // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // disable secondary oscillator
+#pragma config IESO = OFF // disable switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576 // use largest wdt
+#pragma config WINDIS = OFF // use non-window mode wdt
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+
+void LCD_command(unsigned char com) {
+    LATBbits.LATB12 = 0; // DC
+    spi_io(com);
+}
+
+void LCD_data(unsigned char dat) {
+    LATBbits.LATB12 = 1; // DC
+    spi_io(dat);
+}
+
+void LCD_data16(unsigned short dat) {
+    LATBbits.LATB12 = 1; // DC
+    spi_io(dat>>8);
+    spi_io(dat);
+}
+
+void LCD_init() {
+  unsigned int time = 0;
+  LCD_command(ST7789_SWRESET); //software reset
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.15) {}
+  
+  LCD_command(ST7789_SLPOUT); //exit sleep
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.5) {}
+  
+  LCD_command(ST7789_COLMOD);
+  LCD_data(0x55);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.01) {}
+  
+  LCD_command(ST7789_MADCTL);
+  LCD_data(0x00);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.001) {}
+  
+  LCD_command(ST7789_CASET);
+  LCD_data(0x00);
+  LCD_data(ST7789_XSTART);
+  LCD_data((240+ST7789_XSTART)>>8);
+  LCD_data((240+ST7789_XSTART)&0xFF);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.001) {}
+
+  LCD_command(ST7789_RASET);
+  LCD_data(0x00);
+  LCD_data(ST7789_YSTART);
+  LCD_data((240+ST7789_YSTART)>>8);
+  LCD_data((240+ST7789_YSTART)&0xFF);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.001) {}
+  
+  LCD_command(ST7789_INVON);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.01) {}
+
+  LCD_command(ST7789_NORON);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.01) {}
+  
+  LCD_command(ST7789_DISPON);
+  time = _CP0_GET_COUNT();
+  while (_CP0_GET_COUNT() < time + 48000000/2*0.5) {}
+}
+
+void LCD_drawPixel(unsigned short x, unsigned short y, unsigned short color) {
+  // should check boundary first
+  LCD_setAddr(x,y,x+1,y+1);
+  LCD_data16(color);
+}
+
+void LCD_setAddr(unsigned short x0, unsigned short y0, unsigned short x1, unsigned short y1) {
+  LCD_command(ST7789_CASET); // Column
+  LCD_data16(x0+ST7789_XSTART);
+  LCD_data16(x1+ST7789_XSTART);
+  
+  LCD_command(ST7789_RASET); // Page
+  LCD_data16(y0+ST7789_YSTART);
+  LCD_data16(y1+ST7789_YSTART);
+
+  LCD_command(ST7789_RAMWR); // Into RAM
+}
+
+void LCD_clearScreen(unsigned short color) {
+  int i;
+  LCD_setAddr(0,0,_GRAMWIDTH,_GRAMHEIGH);
+	for (i = 0;i < _GRAMSIZE; i++){
+		LCD_data16(color);
+	}
+}
+
+// drawChar function
+void drawChar(unsigned short x, unsigned short y, unsigned short color, unsigned char letter){
+    int col = 0;
+    int row = 0; 
+    letter = letter - 0x20; 
+    for(col = 0; col<5; col++){
+        unsigned char asc = ASCII[letter][col];
+        for (row = 0; row<8; row++){
+            if ((asc>>(7-row))&0b1 == 0b1){ 
+                LCD_drawPixel(x+col,y+(7-row),color); // letter color
+            }else{
+                LCD_drawPixel(x+col,y+(7-row),RED); //background color
+            }
+            
+        }
+    }
+}
+
+#define SIZE 20
+#define SIZE2 100
+void drawString(unsigned short x, unsigned short y,unsigned short color, char* m){
+    int i = 0;
+    while(m[i]){ 
+        unsigned char letter = m[i];
+        drawChar(x+(i*5), y, color, letter);
+        i++ ; 
+    }
+}
+
+
+int main() {
+    
+    __builtin_disable_interrupts(); // disable interrupts while initializing things
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0; //A4 as output
+    TRISBbits.TRISB4 = 1; //B4 as input 
+    
+    LATAbits.LATA4 = 0; 
+    LATBbits.LATB4 = 0; //both A4 and B4 turned off 
+    
+  
+    __builtin_enable_interrupts();
+    
+    initSPI();
+    LCD_init(); 
+    LCD_clearScreen(BLUE); 
+    
+    char m[SIZE];
+    char FPS[SIZE];
+    char bar[SIZE];
+   
+    float start_time; 
+    float end_time; 
+    float total_time; 
+    //char str[] = " ";
+    //char space = ' ';
+    //strncat(str, &space, 100); 
+    
+    _CP0_SET_COUNT(0); 
+    start_time = _CP0_GET_COUNT();
+    
+    sprintf(m, "Hello World!");
+    drawString(10,10, WHITE, m);
+    
+    end_time = _CP0_GET_COUNT(); 
+    total_time = 1/((end_time-start_time)/24000000);
+    
+    sprintf(FPS, "FPS: %2.3f",total_time );
+    drawString(10,40, WHITE, FPS);
+    
+    while(1){
+        int i; 
+        int ii; 
+        for(i =0; i<100; i++){
+            for (ii = 0; ii < 10; ii++){
+                if (i < 50)
+                    LCD_drawPixel(10+i,20+ii,RED);
+                if (i > 50){
+                    LCD_drawPixel(110-i,20+ii,WHITE);
+                }
+            }
+        //drawChar(100,10,WHITE, i);
+        sprintf(bar, "%2d",i);
+        drawString(80,10, WHITE, bar);
+        }
+        
+    }
+    
+    //48mil = 2 seconds
+}
